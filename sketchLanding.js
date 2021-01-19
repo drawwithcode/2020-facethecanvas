49e964bd8bb4d82f0162d59e55fae2e1d28b0699
Filippo-Tortelli <filippo.tortelli@gmail.com>
2021-01-19 15:36:07 +0100

    push data file

--- sketchLanding.js
+++ sketchLanding.js
@@ -1,14 +1,31 @@
 let whichArwork = 1;
+let artwork1;
+let artwork2;
+let artwork3;
+let artwork4;
+
+function preload() {
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background("black");
+
   fill("white");
   textAlign(CENTER);
-  textSize(100);
-  text("Click",width/2,height/2);
+  textSize(30);
+  text("Click",width/2,height/3);
+
+  loadPreview();
 }
-function mouseClicked() {
-  //se questo link viene inserito nella funzione confirmPhoto() ci sono problemi col database, che non riceve la foto
-  window.open('artwork'+whichArwork+'.html', '_self');
+
+function loadPreview() {
+  artwork1 = ellipse(width/5,height/2,50);
+  artwork2 = ellipse(2*width/5,height/2,50);
+  artwork3 = ellipse(3*width/5,height/2,50);
+  artwork4 = ellipse(4*width/5,height/2,50);
+}
+
+function mouseClicked(artwork1) {
+  window.open('artwork1.html', '_self');
 }
